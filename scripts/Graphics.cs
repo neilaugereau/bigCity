a1500768465e9d7fdee4e3f4841ad110ab929079
Neil Augereau <neil.augereau79@gmail.com>
2024-11-12 17:21:38 +0100

    Improve options

---
+++ scripts/Graphics.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class Graphics : Node
+{
+	public static WorldEnvironment worldEnvironment;
+	public static DirectionalLight3D light;
+	public static int lightModeIndex = 2;
+	public static bool lightIsEnabled = true;
+}
