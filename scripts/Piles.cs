a525e2e86d2f5767866404dc313de4ba8e4d03e9
Corentin Jouan <122738788+WalpurgisTime@users.noreply.github.com>
2024-11-12 16:31:51 +0100

    Piles type generic added

--- scripts/Piles.cs
+++ scripts/Piles.cs
@@ -2,33 +2,22 @@ using System.Collections.Generic;
 namespace BigCity.scripts;
 
 
-public class Piles
+public class Piles<T> where T : card_base, new()
 {
-    public card_base cards;
     public int number;
-    public List<card_base> pileCards;
-    public Piles(card_base _cards,int _number)
+
+    public Piles(int _number)
     {
-        cards = _cards;
         number = _number;
-        pileCards = new List<card_base>();
-        for (int i = 0; i < _number; i++)
-        {
-            pileCards.Add(cards);
-        }
     }
 
     public card_base GetCards()
     {
-        if (pileCards.Count > 0)
+        if (number > 0)
         {
-            card_base card = pileCards[0]; 
-            pileCards.RemoveAt(0);       
-            return card;                   
+            number--;
+            return new T(); 
         }
-        return null; 
+        return null;
     }
-    
-    
-    
 }
