32e011e6b8b5685fbef65b721b1f23f2e0882461
Corentin Jouan <122738788+WalpurgisTime@users.noreply.github.com>
2024-11-14 16:54:21 +0100

    Dice

--- scripts/Dice.cs
+++ scripts/Dice.cs
@@ -1,14 +1,17 @@
+
+using Godot;
 using System;
 
 namespace BigCity.scripts;
 
-public class Dice
+public partial class Dice : RigidBody3D
 {
 	Random random = new();
 	
 	public int ThrowDice()
 	{
 		int currentFaces = random.Next(1,6);
+		
 		return currentFaces;
 	}
 }
