aaa23080cb67c6d1ca175954ab6651bd0e8a6bbe
Corentin Jouan <122738788+WalpurgisTime@users.noreply.github.com>
2024-11-15 09:28:04 +0100

    Dice Behaviour done

--- scripts/Dice.cs
+++ scripts/Dice.cs
@@ -4,11 +4,28 @@ using System;
 
 namespace BigCity.scripts;
 
-public partial class Dice : RigidBody3D
+public partial class Dice : Node
 {
 	Random random = new();
+	
+	[Export] private PackedScene objectToSpawn;  // Scène assignable dans l'éditeur
+
+	public override void _Ready()
+	{
+		base._Ready();
+        Node firstObject = objectToSpawn.Instantiate();
+        AddChild(firstObject); 
+        if (firstObject is RigidBody3D firstRigidBody)
+        {
+         	firstRigidBody.Position = new Vector3(0, 5, 0);  
+        }
+        
+		
+	}
+
 	public int ThrowDice()
 	{
+		
 		int currentFaces = random.Next(1,6);
 		
 		return currentFaces;
