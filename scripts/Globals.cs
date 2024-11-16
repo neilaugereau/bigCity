59e1288f6d00af4f8d2a5d818c7917e35d60efbc
Halchimer <halchimer.contact@gmail.com>
2024-11-16 18:38:11 +0100

    Card Renderer V2

--- scripts/Globals.cs
+++ scripts/Globals.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 
 public partial class Globals : Node
 {
-    public static float globalVolume = 100;
-    public static float soundVolume = 100;
-    public static float musicVolume = 100;
+	public static float globalVolume = 100;
+	public static float soundVolume = 100;
+	public static float musicVolume = 100;
 
-    //public static int[,] gridPosition = new int[100, 100];
-    public static Dictionary<Vector2, E_Buildings> gridPositions = new Dictionary<Vector2, E_Buildings>();
-    public enum E_Buildings
-    {
-        FARM,
-        FIELDS,
-        TOWER
-    }
-    
-    
+	//public static int[,] gridPosition = new int[100, 100];
+	public static Dictionary<Vector2, E_Buildings> gridPositions = new Dictionary<Vector2, E_Buildings>();
+	public enum E_Buildings
+	{
+		FARM,
+		FIELDS,
+		TOWER
+	}
+	
+	
 }
