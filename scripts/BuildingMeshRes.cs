31b0284ee1c99f3b4b73d69118c661d7639bb623
Halchimer <halchimer.contact@gmail.com>
2024-11-17 15:05:56 +0100

    Added mesh support for cards

---
+++ scripts/BuildingMeshRes.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class BuildingMeshRes : Resource
+{
+	[Export] public Mesh Farm;
+	[Export] public Mesh Field;
+	[Export] public Mesh Forest;
+	[Export] public Mesh Mine;
+	[Export] public Mesh Orchard;
+	[Export] public Mesh Cafe;
+	[Export] public Mesh Restaurant;
+	[Export] public Mesh Bakery;
+	[Export] public Mesh Supermarket;
+	[Export] public Mesh FurnitureFactory;
+	[Export] public Mesh VegetablesMarket;
+	[Export] public Mesh CheeseShop;
+	[Export] public Mesh TVChannel;
+	[Export] public Mesh Stadium;
+	[Export] public Mesh BusinessCenter;
+	[Export] public Mesh Tower;
+	[Export] public Mesh TrainStation;
+	[Export] public Mesh AmusementPark;
+	[Export] public Mesh Mall;
+}
--- scripts/Globals.cs
+++ scripts/Globals.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 public partial class Globals : Node
 {
+	private BuildingMeshRes buildingsMeshs;
+	
 	public static float globalVolume = 100;
 	public static float soundVolume = 100;
 	public static float musicVolume = 100;
@@ -46,31 +48,33 @@ public partial class Globals : Node
 
 	public Globals()
 	{
+		buildingsMeshs = GD.Load<BuildingMeshRes>("res://assets/buildingMeshs.tres");
+		
 		Building = new Dictionary<E_Building, CardBase>
 		{
-			{E_Building.FARM, new CardBase( (ECardColor)ECardColor.Blue, (ECardType)ECardType.Farm , "Farm", CardDescriptions.Blue(1), 1, (Mesh)this.GetMeta("FarmMesh"), 2, 2) },
-			{E_Building.FIELD, new CardBase( (ECardColor)ECardColor.Blue, (ECardType)ECardType.Agricole , "Wheat Field", CardDescriptions.Blue(1), 0, (Mesh)this.GetMeta("FieldMesh"), 1, 1) },
-			{E_Building.FOREST, new CardBase( (ECardColor)ECardColor.Blue, (ECardType)ECardType.Resources , "Forest", CardDescriptions.Blue(1), 3, (Mesh)this.GetMeta("ForestMesh"), 5, 5) },
-			{E_Building.MINE, new CardBase( (ECardColor)ECardColor.Blue, (ECardType)ECardType.Resources , "Mine", CardDescriptions.Blue(5), 6, (Mesh)this.GetMeta("MineMesh"), 9, 9) },
-			{E_Building.ORCHARD, new CardBase( (ECardColor)ECardColor.Blue, (ECardType)ECardType.Agricole , "Orchard", CardDescriptions.Blue(3), 3, (Mesh)this.GetMeta("OrchardMesh"), 10, 10) },
+			{E_Building.FARM, new CardBase( (ECardColor)ECardColor.Blue, (ECardType)ECardType.Farm , "Farm", CardDescriptions.Blue(1), 1,buildingsMeshs.Farm, 2, 2) },
+			{E_Building.FIELD, new CardBase( (ECardColor)ECardColor.Blue, (ECardType)ECardType.Agricole , "Wheat Field", CardDescriptions.Blue(1), 0, buildingsMeshs.Field, 1, 1) },
+			{E_Building.FOREST, new CardBase( (ECardColor)ECardColor.Blue, (ECardType)ECardType.Resources , "Forest", CardDescriptions.Blue(1), 3, buildingsMeshs.Forest, 5, 5) },
+			{E_Building.MINE, new CardBase( (ECardColor)ECardColor.Blue, (ECardType)ECardType.Resources , "Mine", CardDescriptions.Blue(5), 6, buildingsMeshs.Mine, 9, 9) },
+			{E_Building.ORCHARD, new CardBase( (ECardColor)ECardColor.Blue, (ECardType)ECardType.Agricole , "Orchard", CardDescriptions.Blue(3), 3, buildingsMeshs.Orchard, 10, 10) },
 
-			{E_Building.CAFE, new CardBase( (ECardColor)ECardColor.Red, (ECardType)ECardType.Restauration , "Cafe", "Recieve 1 coin\nfrom the player\nthat threw the dices.", 2, (Mesh)this.GetMeta("CafeMesh"), 3, 3) },
-			{E_Building.RESTAURANT, new CardBase( (ECardColor)ECardColor.Red, (ECardType)ECardType.Restauration , "Restaurant", "Recieve 2 coin\nfrom the player\nthat threw the dices.", 3, (Mesh)this.GetMeta("RestaurantMesh"), 9, 10) },
+			{E_Building.CAFE, new CardBase( (ECardColor)ECardColor.Red, (ECardType)ECardType.Restauration , "Cafe", "Recieve 1 coin\nfrom the player\nthat threw the dices.", 2, buildingsMeshs.Cafe, 3, 3) },
+			{E_Building.RESTAURANT, new CardBase( (ECardColor)ECardColor.Red, (ECardType)ECardType.Restauration , "Restaurant", "Recieve 2 coin\nfrom the player\nthat threw the dices.", 3, buildingsMeshs.Restaurant, 9, 10) },
 
-			{E_Building.BAKERY, new CardBase( (ECardColor)ECardColor.Green, (ECardType)ECardType.Shop , "Bakery", CardDescriptions.GreenBank(1), 0, (Mesh)this.GetMeta("BakeryMesh"), 2, 3) },
-			{E_Building.SUPERMARKET, new CardBase( (ECardColor)ECardColor.Green, (ECardType)ECardType.Shop , "Supermarket", CardDescriptions.GreenBank(3), 2, (Mesh)this.GetMeta("SupermarketMesh"), 4, 4) },
-			{E_Building.FURNITURE_FACTORY, new CardBase( (ECardColor)ECardColor.Green, (ECardType)ECardType.Factory , "Furniture Factory", CardDescriptions.GreenForeach(3),3, (Mesh)this.GetMeta("FurnitureFactoryMesh"), 8, 8) },
-			{E_Building.VEGETABLES_MARKET, new CardBase( (ECardColor)ECardColor.Green, (ECardType)ECardType.Market , "Vegetables Market", CardDescriptions.GreenForeach(2), 2, (Mesh)this.GetMeta("VegetablesMarketMesh"), 11, 12) },
-			{E_Building.CHEESE_SHOP, new CardBase( (ECardColor)ECardColor.Green, (ECardType)ECardType.Factory , "Cheese Shop", CardDescriptions.GreenForeach(3), 5, (Mesh)this.GetMeta("CheeseShopMesh"), 7, 7) },
+			{E_Building.BAKERY, new CardBase( (ECardColor)ECardColor.Green, (ECardType)ECardType.Shop , "Bakery", CardDescriptions.GreenBank(1), 0, buildingsMeshs.Bakery, 2, 3) },
+			{E_Building.SUPERMARKET, new CardBase( (ECardColor)ECardColor.Green, (ECardType)ECardType.Shop , "Supermarket", CardDescriptions.GreenBank(3), 2, buildingsMeshs.Supermarket, 4, 4) },
+			{E_Building.FURNITURE_FACTORY, new CardBase( (ECardColor)ECardColor.Green, (ECardType)ECardType.Factory , "Furniture Factory", CardDescriptions.GreenForeach(3),3, buildingsMeshs.FurnitureFactory, 8, 8) },
+			{E_Building.VEGETABLES_MARKET, new CardBase( (ECardColor)ECardColor.Green, (ECardType)ECardType.Market , "Vegetables Market", CardDescriptions.GreenForeach(2), 2, buildingsMeshs.VegetablesMarket, 11, 12) },
+			{E_Building.CHEESE_SHOP, new CardBase( (ECardColor)ECardColor.Green, (ECardType)ECardType.Factory , "Cheese Shop", CardDescriptions.GreenForeach(3), 5, buildingsMeshs.CheeseShop, 7, 7) },
 
-			{E_Building.TV_CHANNEL, new CardBase( (ECardColor)ECardColor.Violet, (ECardType)ECardType.Special , "TV Channel", CardDescriptions.YOUR_TURN_ONLY + "Recieve 5 coins\nfrom your enemy.", 7, (Mesh)this.GetMeta("TvChannelMesh"), 6, 6) },
-			{E_Building.STADIUM, new CardBase( (ECardColor)ECardColor.Violet, (ECardType)ECardType.Special , "Stadium", CardDescriptions.YOUR_TURN_ONLY + "Recieve 2 coins\nfrom your enemy.", 6, (Mesh)this.GetMeta("StadiumMesh"), 6, 6) },
-			{E_Building.BUSINESS_CENTER, new CardBase( (ECardColor)ECardColor.Violet, (ECardType)ECardType.Special , "Business Center", CardDescriptions.YOUR_TURN_ONLY + "You can exchange\nwith your enemy\nany building\nexcept monuments.", 8, (Mesh)this.GetMeta("BusinessCenterMesh"), 6, 6) },
+			{E_Building.TV_CHANNEL, new CardBase( (ECardColor)ECardColor.Violet, (ECardType)ECardType.Special , "TV Channel", CardDescriptions.YOUR_TURN_ONLY + "Recieve 5 coins\nfrom your enemy.", 7, buildingsMeshs.TVChannel, 6, 6) },
+			{E_Building.STADIUM, new CardBase( (ECardColor)ECardColor.Violet, (ECardType)ECardType.Special , "Stadium", CardDescriptions.YOUR_TURN_ONLY + "Recieve 2 coins\nfrom your enemy.", 6, buildingsMeshs.Stadium, 6, 6) },
+			{E_Building.BUSINESS_CENTER, new CardBase( (ECardColor)ECardColor.Violet, (ECardType)ECardType.Special , "Business Center", CardDescriptions.YOUR_TURN_ONLY + "You can exchange\nwith your enemy\nany building\nexcept monuments.", 8, buildingsMeshs.BusinessCenter, 6, 6) },
 
-			{E_Building.TOWER, new CardBase( (ECardColor)ECardColor.Yellow, (ECardType)ECardType.Monument , "Tower", "Once each round\nyou can choose\nto throw your dice again.", 22, (Mesh)this.GetMeta("FarmMesh"), 0, 0) },
-			{E_Building.TRAIN_STATION, new CardBase( (ECardColor)ECardColor.Yellow, (ECardType)ECardType.Monument , "Train Station", "You can throw\ntwo dices.", 4, (Mesh)this.GetMeta("TrainStationMesh"),0, 0) },
-			{E_Building.AMUSEMENT_PARK, new CardBase( (ECardColor)ECardColor.Yellow, (ECardType)ECardType.Monument , "Amusement Park", "", 16, (Mesh)this.GetMeta("AmusementParkMesh"), 0, 0) },
-			{E_Building.MALL, new CardBase( (ECardColor)ECardColor.Yellow, (ECardType)ECardType.Monument , "Mall", "", 10, (Mesh)this.GetMeta("MallMesh"), 0, 0) },
+			{E_Building.TOWER, new CardBase( (ECardColor)ECardColor.Yellow, (ECardType)ECardType.Monument , "Tower", "Once each round\nyou can choose\nto throw your dice again.", 22, buildingsMeshs.Tower, 0, 0) },
+			{E_Building.TRAIN_STATION, new CardBase( (ECardColor)ECardColor.Yellow, (ECardType)ECardType.Monument , "Train Station", "You can throw\ntwo dices.", 4, buildingsMeshs.TrainStation,0, 0) },
+			{E_Building.AMUSEMENT_PARK, new CardBase( (ECardColor)ECardColor.Yellow, (ECardType)ECardType.Monument , "Amusement Park", "", 16, buildingsMeshs.AmusementPark, 0, 0) },
+			{E_Building.MALL, new CardBase( (ECardColor)ECardColor.Yellow, (ECardType)ECardType.Monument , "Mall", "", 10, buildingsMeshs.Mall, 0, 0) },
 		};
 	}
 	
