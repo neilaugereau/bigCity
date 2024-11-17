98205a5d0583de724096c8c9886f5b9b650f1a31
neilaugereau <neil.augereau79@gmail.com>
2024-11-17 13:39:42 +0100

    added card interface

---
+++ scripts/CardManager.cs
@@ -0,0 +1,162 @@
+using Godot;
+using Godot.Collections;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public partial class CardManager : Node2D
+{
+    const int COLLISION_MASK_CARD = 1;
+
+    private Node2D _cardBeingDragged;
+    private Vector2 _screenSize;
+
+    private bool isHoveringOnCard;
+    public override void _Ready()
+    {
+        _screenSize = GetViewportRect().Size;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseEvent)
+        {
+            if (mouseEvent.ButtonIndex == MouseButton.Left)
+            {
+                if (mouseEvent.Pressed)
+                {
+                    // Detect the card with the mouse
+                    var card = RaycastCheckForCard();
+                    if (card is Node2D cardNode)
+                    {
+                        StartDrag(cardNode);
+                    }
+                }
+                else
+                {
+                    // Dragged the card when the button is Released
+                    FinishDrag();
+                }
+            }
+        }
+    }
+
+    private void StartDrag(Node2D card)
+    {
+        _cardBeingDragged = card;
+        card.Scale = new Vector2(1,1);
+    }
+
+    private void FinishDrag()
+    {
+        _cardBeingDragged.Scale = new Vector2(1.05f, 1.05f);
+        _cardBeingDragged = null;
+    }
+
+    public void ConnectCardsSignals(Card card)
+    {
+        GD.Print("Connecting signals...");
+        card.Connect("hovered", new Callable(this, "on_hover_over_card"));
+        card.Connect("hoveredOff", new Callable(this, "on_hover_off_card"));
+    }
+
+    private void on_hover_over_card(Card card)
+    {
+        if(!isHoveringOnCard)
+        {
+            isHoveringOnCard = true;
+            HighlightCard(card, true);
+        }
+    }
+
+    private void on_hover_off_card(Card card)
+    {
+        if (_cardBeingDragged == null)
+        {
+            HighlightCard(card, false);
+            //Check if hovered off card straight to another card
+            var newCardHovered = RaycastCheckForCard();
+            if (newCardHovered is Card newCard)
+            {
+                HighlightCard(newCard, true);
+            }
+            else
+            {
+                isHoveringOnCard = false;
+            }
+        }
+    }
+
+    private void HighlightCard(Card card, bool hovered)
+    {
+        if(hovered)
+        {
+            card.Scale = new Vector2(1.08f, 1.08f);
+            card.ZIndex = 1;
+        }
+        else
+        {
+            card.Scale = new Vector2(1.0f, 1.0f);
+            card.ZIndex = 0;
+        }
+    }
+
+    private object RaycastCheckForCard()
+    {
+        var spaceState = GetWorld2D().DirectSpaceState;
+
+        var parameters = new PhysicsPointQueryParameters2D
+        {
+            Position = GetGlobalMousePosition(),
+            CollideWithAreas = true,
+            CollisionMask = COLLISION_MASK_CARD
+        };
+
+        var results = spaceState.IntersectPoint(parameters);
+        if (results.Count > 0)
+        {
+            return GetCardWithHighestZIndex(results);
+        }
+        return null;
+    }
+
+    private object GetCardWithHighestZIndex(Array<Dictionary> cards)
+    {
+        // Assume the first card in the array is the one with the highest ZIndex.
+        Area2D highestZCard = (Area2D)cards[0]["collider"].Obj;
+
+        // Try to get the ZIndex of the parent if it's a Node2D; otherwise, use the card's ZIndex.
+        int highestZIndex = highestZCard.GetParent() is Node2D parentNode
+                    ? parentNode.ZIndex
+                    : highestZCard.ZIndex;
+
+        for (int i = 1; i< cards.Count; i++)
+        {
+            if (cards[i]["collider"].Obj is Area2D card)
+            {
+                // Retrieve the ZIndex of the parent if it's a Node2D, otherwise use the card's ZIndex.
+                int parentZIndex = card.GetParent() is Node2D cardParent
+                   ? cardParent.ZIndex
+                   : card.ZIndex;
+
+                if (card.ZIndex > highestZIndex)
+                {
+                    highestZIndex = card.ZIndex;
+                    highestZCard = card;
+                }
+            }
+        }
+        return highestZCard.GetParent();
+    }
+
+
+
+    public override void _Process(double delta)
+    {
+        if (_cardBeingDragged != null)
+        {
+            Vector2 mousePos = GetGlobalMousePosition();
+            // Update the card position with the mouse position
+            _cardBeingDragged.GlobalPosition = new Vector2(Mathf.Clamp(mousePos.X , 0 , _screenSize.X), Mathf.Clamp(mousePos.Y, 0, _screenSize.Y));
+        }
+    }
+}
--- scripts/Physics/GridSelection.cs
+++ scripts/Physics/GridSelection.cs
@@ -9,49 +9,68 @@ public partial class GridSelection : Camera3D
 	public int Spacement = 5;
 	public Vector2 PlaneSize = new Vector2(60, 60); // A changer en fonction de la taille
 	public Vector2 tilePose;
+	
 	public Node floatingTile;
+	private bool isCtrlPressed = false;
+	
 	
 	public override void _Input(InputEvent @event)
 	{
 		base._Input(@event);
-		PhysicsDirectSpaceState3D spaceState = GetWorld3D().DirectSpaceState;
-		Vector2 mousePos = GetViewport().GetMousePosition();
-		Vector3 rayOrigin = ProjectRayOrigin(mousePos);
-		Vector3 rayEnd = rayOrigin + ProjectRayNormal(mousePos) * 200;
-		var rayQuery = new PhysicsRayQueryParameters3D
+		if (@event is InputEventKey keyEvent)
 		{
-			From = rayOrigin,
-			To = rayEnd
-		};
-		Godot.Collections.Dictionary intersection = spaceState.IntersectRay(rayQuery);
-		if(intersection.ContainsKey("collider"))
-		{
-			Vector3 positionToMove = (Vector3)intersection["position"];
-			positionToMove[0] = (float)Math.Round(positionToMove[0] / Spacement) * Spacement;
-			positionToMove[2] = (float)Math.Round(positionToMove[2] / Spacement) * Spacement;
+			if (keyEvent.Keycode == Key.Ctrl && keyEvent.Pressed && !keyEvent.Echo)
+				isCtrlPressed = !isCtrlPressed;
+		}
 
 			tilePose = new Vector2(positionToMove[0], positionToMove[2]);
 			if (Math.Abs(positionToMove[0]) != Math.Abs(PlaneSize[0] / 2) &&
-			    Math.Abs(positionToMove[2]) != Math.Abs(PlaneSize[1] / 2))
+				Math.Abs(positionToMove[2]) != Math.Abs(PlaneSize[1] / 2))
+		if (isCtrlPressed)
+		{
+			PhysicsDirectSpaceState3D spaceState = GetWorld3D().DirectSpaceState;
+			Vector2 mousePos = GetViewport().GetMousePosition();
+			Vector3 rayOrigin = ProjectRayOrigin(mousePos);
+			Vector3 rayEnd = rayOrigin + ProjectRayNormal(mousePos) * 200;
+			var rayQuery = new PhysicsRayQueryParameters3D
 			{
-				
-				if (@event.IsPressed() && @event is InputEventMouseButton)
+				From = rayOrigin,
+				To = rayEnd
+			};
+			Godot.Collections.Dictionary intersection = spaceState.IntersectRay(rayQuery);
+			if(intersection.ContainsKey("collider"))
+			{
+				Vector3 positionToMove = (Vector3)intersection["position"];
+				positionToMove[0] = (float)Math.Round(positionToMove[0] / Spacement) * Spacement;
+				positionToMove[2] = (float)Math.Round(positionToMove[2] / Spacement) * Spacement;
+
+				tilePose = new Vector2(positionToMove[0], positionToMove[2]);
+				if (Math.Abs(positionToMove[0]) != Math.Abs(PlaneSize[0] / 2) &&
+				    Math.Abs(positionToMove[2]) != Math.Abs(PlaneSize[1] / 2))
 				{
-					if (!Globals.gridPositions.ContainsKey(tilePose))
-						PlaceTile(positionToMove, 0,Colors.Black);
-				}
-				else if( @event is InputEventMouseMotion)
-				{
-					if (!Globals.gridPositions.ContainsKey(tilePose))
+					if (@event.IsPressed() && @event is InputEventMouseButton)
 					{
-						TileSelector(positionToMove, 4,Colors.Green);
+						if (!Globals.gridPositions.ContainsKey(tilePose))
+							PlaceTile(positionToMove, 0,Colors.Black);
 					}
-					else
+					else if(@event is InputEventMouseMotion)
 					{
-						TileSelector(positionToMove, 4, Colors.Red);
+						if (!Globals.gridPositions.ContainsKey(tilePose))
+						{
+							TileSelector(positionToMove, 4,Colors.Green);
+						}
+						else
+						{
+							TileSelector(positionToMove, 4, Colors.Red);
+						}
+					
 					}
 				}
 			}
+		}
+		else
+		{
+			DestroyTile();
 		}
 	}
 
@@ -59,7 +78,7 @@ public partial class GridSelection : Camera3D
 	{
 		//GD.Print($"({(positionToMove[0])} ; {(positionToMove[2])})");
 		var building = tiles.Instantiate();
-		Globals.gridPositions.Add(tilePose, Globals.E_Buildings.FARM);
+		Globals.gridPositions.Add(tilePose, Globals.E_Building.FARM);
 		GetTree().GetRoot().AddChild(building);
 		
 		if (building is Node3D buildingNode)
@@ -83,6 +102,7 @@ public partial class GridSelection : Camera3D
 		{
 			floatingTile = tiles.Instantiate();
 			GetTree().GetRoot().AddChild(floatingTile);
+			
 		}
 		
 		if (floatingTile is Node3D floatingTileNode)
@@ -97,5 +117,15 @@ public partial class GridSelection : Camera3D
 			buildingMesh.SetSurfaceOverrideMaterial(0, material);
 			material.AlbedoColor = color;
 		}
+	}
+
+	public void DestroyTile()
+	{
+		if (floatingTile != null)
+		{
+			floatingTile.QueueFree();
+			floatingTile = null;  
+		}
+		
 	}
 }
