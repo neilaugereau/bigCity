64d5ec2eb8fd538ffca59174fa6b3d7a14608998
neilaugereau <neil.augereau79@gmail.com>
2024-11-17 13:59:13 +0100

    card correction

--- scripts/Card.cs
+++ scripts/Card.cs
@@ -1,7 +1,6 @@
 using Godot;
 using System;
 
-<<<<<<< HEAD
 public partial class Card : Node2D
 {
     [Signal]
@@ -34,49 +33,4 @@ public partial class Card : Node2D
 	{
         EmitSignal("hoveredOff", this);
     }
-=======
-public partial class Card : Node
-{
-	public static readonly Color[] CARD_COLORS = new Color[5] {
-		new Color("fcf6a0"),
-		new Color("9275b5"),
-		new Color("f04438"),
-		new Color("65bc6b"),
-		new Color("57a0d7")
-	};
-
-	public static Color GetColorFromCardColor(ECardColor color)
-	{
-		return CARD_COLORS[(int)color];
-	}
-
-	public ECardColor CardColor;
-	public string CardName;
-	public string EffectDescription;
-	public int Price;
-	public int[] DiceRange;
-	public Mesh BuildingMesh;
-	
-	public event EventHandler activationEvent;
-	
-	public Card(ECardColor color, string name, string effectDesc, int price, Mesh mesh, int minDiceRange, int maxDiceRange)
-	{
-		CardColor = color;
-		CardName = name;
-		EffectDescription = effectDesc;
-		Price = price;
-		BuildingMesh = mesh;
-
-		if(minDiceRange == maxDiceRange)
-			DiceRange = new int[1] {minDiceRange};
-		else
-			DiceRange = new int[2] {minDiceRange, maxDiceRange};
-		
-	}
-	
-	public void OnActivate()
-	{
-		activationEvent?.Invoke(this, null);
-	}
->>>>>>> e65426c479e2d33c6bbdf06ee3445486b64768a2
 }
